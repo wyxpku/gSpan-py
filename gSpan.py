340f29a29eb979189615992e569d44c5a811131e
wyxpku <wyx0507@gmail.com>
2016-10-29 14:09:47 +0800

    update

--- gSpan.py
+++ gSpan.py
@@ -1,3 +1,11 @@
+class Vertex():
+	def __init__(self):
+		self.id = 0
+		self.dfs_id = 0
+		self.edges = []
+		self.label = 0
+
+
 class Edge():
 	def __init__(self):
 		self.id = 0
@@ -5,16 +13,13 @@ class Edge():
 		self.vto = 0
 		self.label = 0
 
-class Vertex():
-	def __init__(self):
-		self.edges = []
-		self.label = 0	
-
 class Graph():
 	def __init__(self):
+		self.id = 0
 		self.edges = []
+		self.vertices = []
 
-
+	
 
 class DFS():
 	def __init__(self):
@@ -35,11 +40,13 @@ class PDFS():
 		self.prev = None
 
 class gSpan():
+	id = 0
+	minsup = 0
+
 	def __init__(self):
 		self.id = 0
 		self.minsup = 0
-		self.trans = []
-		self.dfscode = None
-		self.mindfscode = None
-		self.mingraph = None
+		
+		self.
+	
 	
